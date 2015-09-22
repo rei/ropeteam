a8b5fd9c51333a5c54f000baf07cb6c1307ea6c8
Jeff Skjonsby <jskjons@rei.com>
2015-09-22 11:25:20 -0700

    fixing to work with new jgroups version

--- src/main/java/com/rei/ropeteam/Forks.java
+++ src/main/java/com/rei/ropeteam/Forks.java
@@ -2,17 +2,17 @@ package com.rei.ropeteam;
 
 import org.jgroups.JChannel;
 import org.jgroups.fork.ForkChannel;
+import org.jgroups.protocols.FORK;
 import org.jgroups.stack.Protocol;
-import org.jgroups.stack.ProtocolStack;
 
 public class Forks {
     public static ForkChannel fork(JChannel main, String id, Protocol... extraProtocols) {
         try {
-            ForkChannel fork = new ForkChannel(main, id, id, true, 
-                    ProtocolStack.ABOVE, 
-                    main.getProtocolStack().getTopProtocol().getClass(), 
-                    extraProtocols);
+            FORK forkProto = new FORK();
+            forkProto.setProtocolStack(main.getProtocolStack());
+            main.getProtocolStack().insertProtocolAtTop(forkProto);
             
+            ForkChannel fork = new ForkChannel(main, id, id, extraProtocols);
             fork.connect(main.getClusterName());
             
             return fork;
